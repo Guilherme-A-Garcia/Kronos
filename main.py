850146f49e27c293cb16e4f7527fedf2c05c0b59
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-03-10 10:06:03 -0300

    feat(WindowController): add methods to switch windows and inject Controller in views

--- main.py
+++ main.py
@@ -12,14 +12,33 @@ class WindowController:  # receives and manages views' calls and models
         self.current_window = None
         self.root = ctk.CTk()
         self.root.withdraw()
+        
+        self.stopwatch_model = StopwatchModel()
+        self.timer_model = TimerModel()
+        
+        self.show_stopwatch()
+        
+    def withdraw_current(self):
+        if self.current_window is not None:
+            self.current_window.destroy()
+
+    def show_stopwatch(self):
+        self.withdraw_current()
+        self.current_window = StopwatchView(self)
+    
+    def show_timer(self):
+        self.withdraw_current()
+        self.current_window = TimerView(self)
 
 class StopwatchView(ctk.CTkToplevel):  # contains UI
-    def __init__(self):
-        super().__init__()
+    def __init__(self, controller):
+        super().__init__(controller.root)
+        self.controller = controller
     
 class TimerView(ctk.CTkToplevel):  # contains UI
-    def __init__(self):
-        super().__init__()
+    def __init__(self, controller):
+        super().__init__(controller.root)
+        self.controller = controller
         
 class StopwatchModel:  # contains logic independently
     def __init__(self):
